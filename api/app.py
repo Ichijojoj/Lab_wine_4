b9775b3b99ad6ffd5c1e86c0a9532163387d18c8
Ichijojoj <maria.gusseva66@gmail.com>
2026-04-03 17:33:57 +0300

    app fix db

--- api/app.py
+++ api/app.py
@@ -16,7 +16,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.model import WineQualityModel
 
-db = OracleDB()
 
 # Настройка логгера
 logging.basicConfig(level=logging.INFO)
